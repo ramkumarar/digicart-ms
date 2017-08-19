20f2309e7759cf1be442c36cbdaee0db40b45ea9
ramkumar <ramkumar.a@gmail.com>
2017-08-19 17:33:16 +0530

    removed target dir

--- target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO User (id,first_name,last_name) VALUES (1,'shiva','subramaniam');
-INSERT INTO User (id,first_name,last_name) VALUES (2,'ram','ar');
-INSERT INTO User (id,first_name,last_name) VALUES (3,'murugan','arumugaperumal');
-INSERT INTO User (id,first_name,last_name) VALUES (4,'Vaedanth','Ramkumar');
-INSERT INTO User (id,first_name,last_name) VALUES (5,'abdul','kalam');
